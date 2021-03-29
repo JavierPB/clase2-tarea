ae80dbbff377d8375d9a607c459e0ab7cbd65c98
Javier Perez B <javier.perez@ecofor.cl>
2021-03-28 22:11:15 -0300

    Se agrega correo.

--- src/components/contact-form/ContactForm.jsx
+++ src/components/contact-form/ContactForm.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 
 import NameInput from '../name-input/NameInput'
 import envelopeIcon from '../../assets/envelope-vector.svg'
@@ -5,12 +6,21 @@ import styles from './ContactForm.module.css'
 
 
 const ContactForm = () => {
+  const [email, setEmail] = useState('')
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value)
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(event.target.nombres.value)
-    console.log(event.target.apellido_paterno.value)
-    console.log(event.target.apellido_materno.value)
+    const payload = {
+      nombres: event.target.nombres.value,
+      apellido_paterno: event.target.apellido_paterno.value,
+      apellido_materno: event.target.apellido_materno.value,
+      email: event.target.email.value
+    }
+    console.log(payload)
     window.alert('Se ha enviado el formulario')
   }
   const iconStyle = styles.icon + " ml-2"
@@ -23,10 +33,18 @@ const ContactForm = () => {
       <form onSubmit={handleSubmit} className={styles.form}>
         <NameInput />
         <div className="form-group mt-2">
-          <button type="submit" className="btn btn-primary">Enviar Formulario</button>
+          <label>Correo electrónico</label>
+          <input type="email" name="email" id="id_email"
+            onChange={handleEmailChange}
+            value={email}
+            className="form-control"
+            placeholder="Ingrese correo electrónico" />
+        </div>
+        <div className="form-group mt-2">
+          <button type="submit" className="btn btn-primary">Enviar contacto</button>
         </div>
       </form>
-
+      <p></p>
     </div>
 
   )
